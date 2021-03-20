3eba4ecf0e559119731be0db7112dff3b098b087
Tantely Andrianarivola <tantely.and@onja.org>
2021-03-20 15:05:19 +0100

    fixed image sliders

--- webroot/script.js
+++ webroot/script.js
@@ -1,7 +1,10 @@
 //these are the parent elements
 const images = document.querySelector(".images");
 const pictures = images.querySelectorAll("picture");
+const picturesContainer = document.querySelector(".pictures-container");
 const bigImage = images.querySelector(".big-image");
+const prevButton = document.querySelector(".btn-prev");
+const nextButton = document.querySelector(".btn-next");
 
 //this function generates the images to move around.
 function showImage (e) {
@@ -28,3 +31,64 @@ function showImage (e) {
 }
 //event listener
 pictures.forEach(picture => picture.addEventListener("click", showImage))
+
+//function Slider(slider) {
+//    if (!slider instanceof Element) {
+//        throw new Error('No slider passed in');
+//    }
+//    // creat some variables for working with the slider
+//    prevButton.addEventListener('click', () => move('back'));
+//    nextButton.addEventListener('click', move);
+//    startSlider();
+//    applyClasses();
+//}
+//const mySlider = Slider(document.querySelector('.images'));
+
+//function sliderImages () {
+//    function startSlider () {
+//        current = slider.querySelector('.current') || picturesContainer.firstElementChild;
+//        prev = current.previousElementSibling || picturesContainer.lastElementChild;
+//        next = current.nextElementSibling || picturesContainer.firstElementChild;
+//    }
+  
+//    function applyClasses() {
+//        current.classList.add('current');
+//        prev.classList.add('prev');
+//        next.classList.add('next');
+//    }
+  
+//  function move (direction) {
+//    let current;
+//    let prev;
+//    let next;
+//    // first part of the move function, delete all the classes from the element
+//      const classesToRemove = ['prev', 'current', 'next'];
+//      prev.classList.remove(...classesToRemove);
+//      current.classList.remove(...classesToRemove);
+//      next.classList.remove(...classesToRemove);
+//      if (direction === "back") {
+//          // Swap the variable when we go backwards
+//          [prev, current, next] = [
+//              prev.previousElementSibling || picturesContainer.lastElementChild,
+//              prev,
+//              current
+//          ];
+//      } else {
+//          // Do the opposite if we go forwards
+//          [prev, current, next] = [
+//              current,
+//              next.
+//              nextElementSibling || picturesContainer.firstElementChild,
+//          ];
+//      }
+//      applyClasses();
+//  }
+//  prevButton.addEventListener("click", () => {
+
+//  })
+//}
+
+
+//nextButton.addEventListener("click", () => {
+  
+//})
